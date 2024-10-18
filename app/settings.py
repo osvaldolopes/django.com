67b0ca4c368768fa10137af2ddd5b0972b034cf3
Osvaldo Filho <osvaldo_filho@live.com>
2024-10-18 01:07:38 -0300

    create views

--- app/settings.py
+++ app/settings.py
@@ -124,5 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# PASTA PARA UPLOAD DE IMG 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
