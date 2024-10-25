0c13208b271cf304bde9b114d7715526bdf4b89e
bRprPzXS7eCjd2L <osvaldo_filho@live.com>
2024-10-24 23:38:38 -0300

    view login

--- cars/forms.py
+++ cars/forms.py
@@ -28,3 +28,17 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = "__all__"
+    
+    def clean_value(self):
+        value =  self.cleaned_data.get('value')
+        if (value < 20000):
+            self.add_error('value', 'Valor minimo do carro deve ser maior que R$20.000,00')  
+        return value 
+    
+    def clean_factory_year(self):
+        factory_year =  self.cleaned_data.get('factory_year')
+        if (factory_year < 1975):
+            self.add_error('factory_year', 'Valor minimo do ano deve ser maior que 1975')  
+        return factory_year
+    
+    
